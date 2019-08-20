44e253b12181b87fdfea49ee887eea3e8c14e741
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2019-08-20 14:10:20 +0200

    chore(webpack): add sass

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./styles.scss"
+import { test } from "./app/main"
+console.log(test);
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
         test: /.(js|jsx)$/,
         include: [path.resolve(__dirname, 'src')],
         loader: 'babel-loader',
